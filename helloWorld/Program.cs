fd5a977ff285ddc218126a321e3da53c4ecf5576
jsales3 <jsales3>
2018-01-22 11:27:35 -0500

    Initial Commit

---
+++ helloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(42);
+            Console.WriteLine(1.23);
+            Console.WriteLine(2+3);
+
+
+
+
+
+
+        }
+    }
+}
