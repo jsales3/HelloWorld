00944d61cd1bb0324d524d98c092565cf57b8178
jsales3 <jsales3>
2018-01-22 23:26:19 -0500

    working with arrays and data structures

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -10,15 +10,180 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(42);
-            Console.WriteLine(1.23);
-            Console.WriteLine(2+3);
+            //part one grettings statement
+            string messageOne = "Hello World!";
+            string messageTwo = "I AM SPARTACUS!!!!!";
+
+            //Part set initial Spartucus Attributes
+            int ageOne = 35;
+            int ageTwo = 45;
+            int ageThree = 80;
+            double heightOne = 72.50;
+            ageTwo = 70;
+            double heightTwo = 35.12345f;
+            float weightOne = 210.0284606f;
+            float weightTwo = -429.1234573f;
+            bool isGodLike = true;
+            bool isGodLikeTwo = false;
+            char genderMale = 'M';
+            char genderFemale = 'F';
+
+            //Display variables values
+            //Console.WriteLine(messageOne);
+            //Console.WriteLine(messageTwo);
+
+            //Part four mathematical operator, bool and char examples
+            //Console.WriteLine(ageOne + ageTwo);
+            //Console.WriteLine(heightOne - heightTwo);
+            //Console.WriteLine(weightOne * weightTwo);
+            //Console.WriteLine(ageTwo / ageOne);
+            //Console.WriteLine(ageTwo % ageOne);
+            //Console.WriteLine(ageThree / ageOne);
+            //Console.WriteLine(ageThree % ageOne);
+            //Console.WriteLine(isGodLike);
+            //Console.WriteLine(genderMale);
+
+            //Part five variable assignment
+            //ageTwo++;
+            //Console.WriteLine(ageTwo);
+            //ageTwo--;
+            //Console.WriteLine(ageTwo);
+            //Console.WriteLine("5" == "5");
+            //Console.WriteLine(40 != 40);
+            //Console.WriteLine(11 < 4);
+
+            //Part eight, concatenation example
+            //Console.WriteLine(messageOne + " " + messageTwo + ".");
+            //Console.WriteLine("I am {0} years old and yes it's {1} I am godlike", ageOne, isGodLike);
+            //Console.WriteLine("I weigh around {0} ,and I am around {1} intches tall", weightOne,heightOne);
+
+            //Console.WriteLine(ageTwo.Equals(45));
+            //Console.WriteLine(messageTwo.Equals("I am Spartacus"));
+
+            //string myGreeting = "Hello";
+            //Console.WriteLine(myGreeting.ToUpper());
+            //Console.WriteLine(myGreeting.ToLower());
+            //Console.WriteLine(myGreeting);
+
+            //int messOneLength = messageOne.Length;
+            //int messTwoLength = messageTwo.Length;
+
+            //Console.WriteLine("The length of messageOne is " + messageOne.Length);
+            //Console.WriteLine("The length of messageTwo is " + messageTwo.Length);
+            //Console.WriteLine("The length of messageOne is " + messOneLength);
+            //Console.WriteLine("The length of messageTwo is " + messTwoLength);
 
 
+            //string city1 = "vesuvius";
+            //string city2 = "nola";
+            //string city3 = "Nuceria";
+            //int city1Distance, city2Distance, city3Distance;
 
+            ////Solicit user Input example
+            //Console.WriteLine("What is the distance to {0}?", city1);
+            //city1Distance = int.Parse(Console.ReadLine());
+            //Console.WriteLine("what is the distance to " + city2 + "?");
+            //city2Distance = int.Parse(Console.ReadLine());
+            //Console.WriteLine("what is the distance to Nuceria?");
+            //city3Distance = int.Parse(Console.ReadLine());
 
+            //if ((city1Distance <= 125) && ((city1Distance < city2Distance) && (city1Distance < city3Distance)))
+            //{
+            //    Console.WriteLine("we will march to Vesuvius");        
+            //}
+            //if ((city2Distance <= 125) && ((city2Distance < city1Distance) && (city2Distance < city3Distance)))
+            //{
+            //    Console.WriteLine("we will march to Nola");
+            //}
+            //if ((city3Distance <= 125) && ((city3Distance < city1Distance) && (city3Distance < city2Distance)))
+            //{
+            //    Console.WriteLine("we will march to Nuceria");
+            //}
 
+            //Console.WriteLine("what is your rank soldier?");
+            //string rank = Console.ReadLine();
+
+            //Console.WriteLine("what is your age soldier?");
+            //int age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("what is your job soldier?");
+            //string job = Console.ReadLine();
+
+            //if ((rank == "officer") || (age <= 26))
+            //{
+            //    Console.WriteLine("Get your gear, kiss your family goodbye, put on your marching boots.");
+
+            //}
+            //else if ((job == "cook") || (age >= 26))
+            //{
+            //    Console.WriteLine("My army has to eat. Pack your pots and pans");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sorry you are staying home.");
+            //}
+
+            //switch (job)
+            //{
+            //    case "infantry":
+            //        Console.WriteLine("You will carry a sword");
+            //        break;
+
+            //    case "archers":
+            //        Console.WriteLine("You will carry a bow and arrow");
+            //        break;
+
+            //    case "supply":
+            //        Console.WriteLine("You will carry pots and pans");
+            //        break;
+
+            //    case "specialist":
+            //        Console.WriteLine("You will ride horseback");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("You will ride horseback");
+            //        break;
+            //}
+
+            string[] foodList = new string[5];
+            foodList[0] = "Milk";
+            foodList[1] = "Fruit";
+            foodList[2] = "Meat";
+            foodList[3] = "Wine";
+            foodList[4] = "Bread";
+
+            Console.WriteLine(foodList[0]);
+            Console.WriteLine(foodList[1]);
+            Console.WriteLine(foodList[2]);
+            Console.WriteLine(foodList[3]);
+            Console.WriteLine(foodList[4]);
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, ", foodList[0], foodList[1], foodList[2], foodList[3], foodList[4]);
+
+            int[] foodAmount = new int[5];
+            foodAmount[0] = 1000;
+            foodAmount[1] = 100;
+            foodAmount[2] = 2000;
+            foodAmount[3] = 10000;
+            foodAmount[4] = 1500;
+
+            //Console.WriteLine("{0}, {1}, {2}, {3}, {4}, ", foodAmount[0], foodAmount[1], foodAmount[2], foodAmount[3], foodAmount[4]);
+            Console.WriteLine(foodList[0] + ":" + foodAmount[1] + " " + foodList[1] + ":" + foodAmount[1] + " " + foodList[2] + ":" + foodAmount[2] + " " + foodList[3] + ":" + foodAmount[3] + " " + foodList[4] + ":" + foodAmount[4]);
+            Console.WriteLine(foodList.Length);
+
+            string[] elements = messageTwo.Split(' ');
+            Console.WriteLine(elements[0]);
+            Console.WriteLine(elements[1]);
+            Console.WriteLine(elements[2]);
+
+            string myName = "Sue";
+            char[] letters = myName.ToCharArray();
+
+            Console.WriteLine("{0}{1}{2}" , letters[0], letters[1], letters[2]);
+            //Console.WriteLine("{0}{1}{2}", letters[0], letters[1], letters[2]);
 
         }
+
     }
 }
